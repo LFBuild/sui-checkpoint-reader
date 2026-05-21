f616147edcb31d9fd3ef530dfb95423c1cac658f
Alexey <komalex203@gmail.com>
2026-05-21 23:56:21 +0300

    update

--- src/bcs-generator.js
+++ src/bcs-generator.js
@@ -2,7 +2,7 @@ import { writeFile } from 'fs/promises'
 
 import { parse } from 'yaml'
 
-const VERSION = 'testnet-v1.63.2'
+const VERSION = 'testnet-v1.72.2'
 const YAML_URL =
   'https://raw.githubusercontent.com/MystenLabs/sui/{version}/crates/sui-core/tests/staged/sui.yaml'
 
